bcd077e5ec754ee26ce92b3c31720552aefc1457
Guilherme de Paula <78801502+Dipaula2@users.noreply.github.com>
2024-05-07 00:41:46 -0300

    Update Hotel.java

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,10 +1,7 @@
 import java.util.Random;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.Condition;
 
