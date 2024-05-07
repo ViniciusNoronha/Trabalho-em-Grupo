9454701225c59e48bc015eb45a1c58c17c01af29
Guilherme de Paula <78801502+Dipaula2@users.noreply.github.com>
2024-05-06 22:24:29 -0300

    Recepcionista.java

--- src/Recepcionista.java
+++ src/Recepcionista.java
@@ -1,17 +1,32 @@
+import java.util.HashSet;
+
 public class Recepcionista extends Thread {
     private int id;
     private Hotel hotel;
+    private HashSet<Integer> chavesRecebidas = new HashSet<>();
 
     public Recepcionista(int id, Hotel hotel) {
         this.id = id;
         this.hotel = hotel;
     }
 
+    public synchronized void receberChave(Quarto quarto) {
+        chavesRecebidas.add(quarto.getNumero());
+        System.out.println("Recepcionista " + id + " recebeu a chave do quarto " + quarto.getNumero());
+    }
+
+    public synchronized void devolverChave(Quarto quarto) {
+        if (chavesRecebidas.contains(quarto.getNumero())) {
+            chavesRecebidas.remove(quarto.getNumero());
+            System.out.println("Recepcionista " + id + " devolveu a chave do quarto " + quarto.getNumero() + " ao hóspede");
+        }
+    }
+
     @Override
     public void run() {
         try {
             while (true) {
-                // Recepcionistas ficam à disposição para atender novos hóspedes ou gerenciar chaves
+                // Lógica existente
                 hotel.atenderSolicitacoes();
                 Thread.sleep(500);  // Simula o tempo entre atendimentos
             }
